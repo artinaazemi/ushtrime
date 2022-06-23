7026fd10e87dbaf21c48a75f208b8f0800e52773
arti <rea.jashari@ubt-uni.net>
2022-06-23 11:52:46 +0200

    changes

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function Metoda() {
     const Personi={
-        emri: document.getElementById('emri').value,
         email: document.getElementById('email').value,
         password: document.getElementById('password').value,
     }
-    var vargu=[Personi.emri, Personi.email, Personi.password]
-    for(leti=0; i<vargu.length; i++){
+    var vargu=[Personi.email, Personi.password]
+    for(let i=0; i<vargu.length; i++){
         console.log(vargu[i]);
     }
 }
